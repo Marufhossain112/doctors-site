80e5532539e3648331d10b32a5a2150f7d54dca4
maruf <dev.maruf112@gmail.com>
2022-11-23 03:48:08 +0600

    initial set up of stripe on node

--- index.js
+++ index.js
@@ -197,6 +197,18 @@ async function run() {
     const result = await bookingsCollection.findOne(query);
     res.send(result);
   });
+  app.post("/create-payment-intent", async (req, res) => {
+    const price = req.body.price;
+    const amount = price * 100;
+  });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 }
 run().catch((err) => console.log(err));
 
